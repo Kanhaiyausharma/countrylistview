bf1a3641641c807462b7e7bc49f8cb8bd17ce861
kanhaiya <kanaiya3112@gmail.com>
2018-04-27 10:02:35 +0530

    done list view without test

--- app/src/main/java/com/example/user/countrylist/MainActivity.java
+++ app/src/main/java/com/example/user/countrylist/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.user.countrylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView simplelist;
+    String arraylist[]={"India","USA","Germany","UAE","Nepal","Switzerland","London","Canada",
+            "Germany","France","south koria","South africa","Japan","China"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        simplelist =(ListView) findViewById(R.id.listviewcontainer);
+        ArrayAdapter<String> arrayAdapter =new ArrayAdapter<String>(this,R.layout.listview,R.id
+                .textviewobj,arraylist);
+        simplelist.setAdapter(arrayAdapter);
     }
 }
